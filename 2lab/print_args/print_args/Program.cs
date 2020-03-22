3177e014f92f1689f80b74e8bc1a39fd2103a323
OVERENSAL <mle8@yandex.ru>
2020-03-22 12:55:58 +0300

    add 2 lab

---
+++ 2lab/print_args/print_args/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace print_args
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                Console.WriteLine("Number of arguments: " + args.Length);
+                Console.Write("Arguments: ");
+                foreach (string str in args)
+                {
+                    Console.Write(str + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("No parameters were specified!");
+            }
+        }
+    }
+}
---
+++ 2lab/remove_duplicates/remove_duplicates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (checkArguments(args) == 1)
+            {
+                Console.WriteLine(args[0].Distinct().ToArray());
+            }
+        }
+
+        static int checkArguments(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect number of arguments! ");
+                return 0;
+            }
+        }
+    }
+}
