1aab6e935dca59239f262fe64bcb795dfc927f14
OVERENSAL <mle8@yandex.ru>
2020-03-29 13:11:04 +0300

    added 3 lab 1 task

---
+++ 3lab/CheckIdentifier/CheckIdentifier.Tests/ArgumentVerificationTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CheckIdentifier.Tests
+{
+    public class ArgumentVerificationTests
+    {
+        [Fact]
+        public void ArgumentVerification_ShouldVerificationALittleString()
+        {
+            Assert.Equal(-1, CheckIdentifier.ArgumentVerification("g"));
+        }
+
+        [Fact]
+        public void ArgumentVerification_ShouldVerificationString()
+        {
+            Assert.Equal(-1, CheckIdentifier.ArgumentVerification("great"));
+        }
+
+        [Fact]
+        public void ArgumentVerification_ShouldVerificatiotStringWithNumberInEnd()
+        {
+            Assert.Equal(-1, CheckIdentifier.ArgumentVerification("great4"));
+        }
+
+        [Fact]
+        public void ArgumentVerification_ShouldVerificatiotStringWithSomeNumberInEnd()
+        {
+            Assert.Equal(-1, CheckIdentifier.ArgumentVerification("great44"));
+        }
+
+        [Fact]
+        public void ArgumentVerification_NotShouldVerificatiotStringWithNumberInStart()
+        {
+            Assert.Equal(0, CheckIdentifier.ArgumentVerification("4great44"));
+        }
+
+        [Fact]
+        public void ArgumentVerification_NotShouldVerificatiotStringOnlyWithNumberInStart()
+        {
+            Assert.Equal(0, CheckIdentifier.ArgumentVerification("4"));
+        }
+
+        [Fact]
+        public void ArgumentVerification_NotShouldVerificatiotStringWithSpace()
+        {
+            Assert.Equal(2, CheckIdentifier.ArgumentVerification("gr eat"));
+        }
+
+        [Fact]
+        public void ArgumentVerification_NotShouldVerificatiotStringWithSpaceInStart()
+        {
+            Assert.Equal(0, CheckIdentifier.ArgumentVerification(" gr eat"));
+        }
+    }
+}
---
+++ 3lab/CheckIdentifier/CheckIdentifier/CheckIdentifier.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIdentifier
+{
+    public class CheckIdentifier
+    {
+        private const string ERROR_MSG = "Pass one argument to the program for verification.";
+        private const string VALID_SUCCESS = "Yes! Identificator validation was successful!";
+        private const string VALID_FAIL = "No! Identificator validation failed. Unexpected symbol '";
+
+        public static void Main(string[] args)
+        {
+            if (args.Length == 1 && args[0].Length != 0)
+            {
+                string verificationString = args[0];
+                int verificationResult = ArgumentVerification(verificationString);
+                if (verificationResult == -1)
+                {
+                    Console.WriteLine(VALID_SUCCESS);
+                }
+                else
+                {
+                    Console.WriteLine(VALID_FAIL + verificationString[verificationResult] + "' in the position " + verificationResult + ".");
+                }
+            }
+            else
+            {
+                Console.WriteLine(ERROR_MSG);
+            }
+        }
+
+        public static int ArgumentVerification(string verificationString)
+        {
+            int resultIndex = -1;
+            bool valid = true;
+            if (!char.IsLetter(verificationString[0]))
+            {
+                valid = false;
+                resultIndex = 0;
+            }
+            for (int i = 1; i < verificationString.Length && valid; i++)
+            {
+                if (!(char.IsLetter(verificationString[i]) || char.IsDigit(verificationString[i])))
+                {
+                    resultIndex = i;
+                    valid = false;
+                }
+            }
+
+            return resultIndex;
+        }
+    }
+}
