c8f2301d7d5258b2ef651b504f504be508adf28b
OVERENSAL <mle8@yandex.ru>
2020-04-01 20:24:04 +0300

    added removeextrablanks

---
+++ 3lab/RemoveExtraBlanks/RemoveExtraBlanks/RemoveExtraBlanks.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveExtraBlanks
+{ 
+    public class RemoveExtraBlanks
+    {
+        public const string INPUT_FILE_IS_NOT_EXIST = "Input file is not exist";
+        public const string OUTPUT_FILE_IS_NOT_EXIST = "Output file is not exist";
+        public const string USAGE_METHOD = "Usage: RemoveExtraBlanks.exe <input file> <output file>";
+        public static void Main(string[] args)
+        {
+            if (CheckCorrectFiles(args) == 1)
+            {
+                WriteRemoveExtraBlanksStringInFile(args);
+            }
+
+        }
+
+        public static void WriteRemoveExtraBlanksStringInFile(string[] args)
+        {
+            string str;
+            using (StreamReader reader = new StreamReader(args[0]))
+            {
+                using (StreamWriter writer = new StreamWriter(args[1]))
+                {
+                    while ((str = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine(RemoveExtraBlanksInString(str));
+                    }
+                }
+            }
+        }
+
+        public static string RemoveExtraBlanksInString(string str)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(str, "\\s+|\\t+", " ").Trim();
+        }
+
+        public static int CheckCorrectFiles(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                bool input = File.Exists(args[0]);
+                bool output = File.Exists(args[1]);
+                if (input && output)
+                {
+                    return 1;
+                }
+                else
+                {
+                    if (!input)
+                    {
+                        Console.WriteLine(INPUT_FILE_IS_NOT_EXIST);
+                    }
+                    if (!output)
+                    {
+                        Console.WriteLine(OUTPUT_FILE_IS_NOT_EXIST);
+                    }
+                    Console.WriteLine(USAGE_METHOD);
+                    return 0;
+                }
+            }
+            Console.WriteLine(USAGE_METHOD);
+            return 0;
+        }
+    }
+}
---
+++ 3lab/RemoveExtraBlanks/RemoveExtraBlanksTests/RemoveExtraBlanksTests.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace RemoveExtraBlanks.Tests
+{
+    public class CheckCorrectFilesTests
+    {
+        [Fact]
+        public void CheckCorrectFiles_BothFilesAreNotExist_ShouldShowErrorMessage()
+        {
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                string[] args = { };
+                RemoveExtraBlanks.CheckCorrectFiles(args);
+                Assert.Equal(RemoveExtraBlanks.USAGE_METHOD + "\r\n", writer.ToString());
+            }
+        }
+
+        [Fact]
+        public void CheckCorrectFiles_InputIsNotExist_ShouldShowErrorMessage()
+        {
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                string[] args = {"", "output.txt" };
+                int expected = 0;
+                int result = RemoveExtraBlanks.CheckCorrectFiles(args);
+                Assert.Equal(expected, result);
+                Assert.Equal(RemoveExtraBlanks.INPUT_FILE_IS_NOT_EXIST + "\r\n" + RemoveExtraBlanks.USAGE_METHOD + "\r\n", writer.ToString());
+            }
+        }
+
+        [Fact]
+        public void CheckCorrectFiles_OutputIsNotExist_ShouldShowErrorMessage()
+        {
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                string[] args = { "input.txt", "" };
+                int expected = 0;
+                int result = RemoveExtraBlanks.CheckCorrectFiles(args);
+                Assert.Equal(expected, result);
+                Assert.Equal(RemoveExtraBlanks.OUTPUT_FILE_IS_NOT_EXIST + "\r\n" + RemoveExtraBlanks.USAGE_METHOD + "\r\n", writer.ToString());
+            }
+        }
+
+        [Fact]
+        public void CheckCorrectFiles_InputAndOutputAreExist_ShouldReturn1()
+        {
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                string[] args = { "input.txt", "output.txt" };
+                int expected = 1;
+                int result = RemoveExtraBlanks.CheckCorrectFiles(args);
+                Assert.Equal(expected, result);
+            }
+        }
+    }
+
+    public class RemoveExtraBlanksInString
+    {
+        [Fact]
+        public void RemoveExtraBlanksInString_WithTabulationInStart()
+        {
+            Assert.Equal("gg", RemoveExtraBlanks.RemoveExtraBlanksInString("        gg"));
+        }
+
+        [Fact]
+        public void RemoveExtraBlanksInString_WithTabulationInEnd()
+        {
+            Assert.Equal("gg", RemoveExtraBlanks.RemoveExtraBlanksInString("gg      "));
+        }
+
+        [Fact]
+        public void RemoveExtraBlanksInString_WithTabulationInStartAndEnd()
+        {
+            Assert.Equal("gg", RemoveExtraBlanks.RemoveExtraBlanksInString("        gg      "));
+        }
+
+        [Fact]
+        public void RemoveExtraBlanksInString_WithTabulationInStartWithSpaces()
+        {
+            Assert.Equal("gg", RemoveExtraBlanks.RemoveExtraBlanksInString("          gg"));
+        }
+
+        [Fact]
+        public void RemoveExtraBlanksInString_WithTabulationInStartAndEndWithSpaces()
+        {
+            Assert.Equal("gg", RemoveExtraBlanks.RemoveExtraBlanksInString("            gg              "));
+        }
+
+        [Fact]
+        public void RemoveExtraBlanksInSomeWord_WithTabulationInStartAndEndWithSpaces()
+        {
+            Assert.Equal("gg wp", RemoveExtraBlanks.RemoveExtraBlanksInString("            gg                wp     "));
+        }
+    }
+
+    public class WriteRemoveExtraBlanksStringInFile
+    {
+        [Fact]
+        public void WriteRemoveExtraBlanksStringInFile_VoidFile()
+        {
+            string[] args = { "emptyInput.txt", "emptyOutput.txt" };
+            RemoveExtraBlanks.WriteRemoveExtraBlanksStringInFile(args);
+            string expected = null;
+            using (StreamReader reader = new StreamReader(args[1]))
+            {
+                Assert.Equal(expected, reader.ReadLine());
+            }
+        }
+
+        [Fact]
+        public void WriteRemoveExtraBlanksStringInFile_HasLine()
+        {
+            string[] args = { "hasLineInput.txt", "hasLineOutput.txt" };
+            RemoveExtraBlanks.WriteRemoveExtraBlanksStringInFile(args);
+            string expected = "asfsda dsf sdf fds ds d ds";
+            using (StreamReader reader = new StreamReader(args[1]))
+            {
+                Assert.Equal(expected, reader.ReadLine());
+            }
+        }
+    }
+
+    public class Main
+    {
+        [Fact]
+        public void Main_GiveIncorrect2Arguments_ShouldShowMistakeText()
+        {
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                string[] args = { "3", "2" };
+                RemoveExtraBlanks.Main(args);
+                Assert.Equal(RemoveExtraBlanks.INPUT_FILE_IS_NOT_EXIST + "\r\n" + RemoveExtraBlanks.OUTPUT_FILE_IS_NOT_EXIST + "\r\n" + RemoveExtraBlanks.USAGE_METHOD + "\r\n", writer.ToString());
+            }
+        }
+
+        [Fact]
+        public void Main_GiveIncorrectArguments_ShouldShowMistakeText()
+        {
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                string[] args = { "input.txt", "2" };
+                RemoveExtraBlanks.Main(args);
+                Assert.Equal(RemoveExtraBlanks.OUTPUT_FILE_IS_NOT_EXIST + "\r\n" + RemoveExtraBlanks.USAGE_METHOD + "\r\n", writer.ToString());
+            }
+        }
+
+        [Fact]
+        public void Main_GiveIncorrectNumberOfArguments_ShouldShowMistakeText()
+        {
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                string[] args = { };
+                RemoveExtraBlanks.Main(args);
+                Assert.Equal(RemoveExtraBlanks.USAGE_METHOD + "\r\n", writer.ToString());
+            }
+        }
+
+        [Fact]
+        public void Main_GiveVoidInput_ShouldGiveVoidOutput()
+        {
+            string[] args = { "emptyInput.txt", "emptyOutput.txt" };
+            RemoveExtraBlanks.Main(args);
+            string expected = null;
+            using (StreamReader reader = new StreamReader(args[1]))
+            {
+                Assert.Equal(expected, reader.ReadLine());
+            }
+        }
+
+        [Fact]
+        public void Main_GiveInput_ShouldGiveCorrectOutput()
+        {
+            string[] args = { "hasLineInput.txt", "hasLineOutput.txt" };
+            RemoveExtraBlanks.Main(args);
+            string expected = "asfsda dsf sdf fds ds d ds";
+            using (StreamReader reader = new StreamReader(args[1]))
+            {
+                Assert.Equal(expected, reader.ReadLine());
+            }
+        }
+    }
+}
+
